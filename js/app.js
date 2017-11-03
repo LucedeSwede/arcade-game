32c95f74f7d40d10b2140fd930632f422462c479
lucedeswede <nathanielcarolina@gmail.com>
2017-11-03 20:42:16 +0800

    docs: Add comments to js file

--- js/app.js
+++ js/app.js
@@ -1,36 +1,41 @@
 // Enemies our player must avoid
 class Enemy {
     constructor() {
-//        let randomPosX = () => ((Math.random() * 200) - 320);//Math.random() * (-120 - -520) + -520;//-120 -520;
-//        let randomPosX = function() {
-//            return (Math.random() * 200) - 320;
-//        };
+        // The image/sprite for our enemies, using a provided helper to easily load images
         this.sprite = 'images/enemy-bug.png';
-        this.x = this.randomPosX();//(Math.random() * 200) - 320;
-        this.y = this.randomPosY();//[60, 143, 227][Math.floor(Math.random() * 3)];
-        this.speed = this.randomSpeed();//[50, 100, 200, 300][Math.floor(Math.random() * 4)];
+        this.x = this.randomPosX();
+        this.y = this.randomPosY();
+        this.speed = this.randomSpeed();
     }
 
-//    function getRandomArbitrary(min, max) {
-//      return Math.random() * (max - min) + min;
-//    }
-
+    //  Return a random number between min and max, from
+    //  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    //
+    //  function getRandomArbitrary(min, max) {
+    //      return Math.random() * (max - min) + min;
+    //  }
+    //
+    //  Pass in -920 as min value, and -120 as max value
+    //  to have greater variation in x starting positions
     randomPosX() {
         return ((Math.random() * 800) - 920);
     }
 
+    //  Choose random enemy position
+    //  (either one of the lines for the three stone blocks)
     randomPosY() {
         return [59, 143, 227][Math.floor(Math.random() * 3)];
     }
 
+    //  Return a random number between 50 and 200
     randomSpeed() {
         return ((Math.random() * 150) + 50);
-//        return [50, 100, 200, 300][Math.floor(Math.random() * 4)];
     }
 
+    //  Update the enemy's position
+    //  Parameter: dt, a time delta between ticks, multiplied to movement parameter
+    //  to ensure the game runs at the same speed for all computers.
     update(dt) {
-//        this.x += speed;//-120 -420;
-//        this.x += this.speed * dt;
         if (this.x > 550) {
             this.x -= 850;
             this.y = this.randomPosY();
@@ -38,48 +43,26 @@ class Enemy {
         } else {
             this.x += this.speed * dt;
         }
-//        console.log(this.x);
-//        (this.x > 550) ? (this.x = this.x - 850) : (this.x += this.speed * dt);
-
     }
 
+    //  Draw the enemy on the screen
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
-//var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-//    this.sprite = 'images/enemy-bug.png';
-//};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-//Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-//};
-
-// Draw the enemy on the screen, required method for game
-//Enemy.prototype.render = function() {
-//    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-//};
-
 class Player {
     constructor() {
+        //  Set initial position to bottom center
         this.initialX = 200;
         this.initialY = 375;
         this.sprite = 'images/char-boy.png';
         this.x = this.initialX;
         this.y = this.initialY;
-//        this.speed = 100;
     }
 
+    //  Check collisions: check all enemy positions vis-a-vis player position
+    //  and send player back to initial position if collided
     update() {
         allEnemies.forEach(function(enemy) {
             if (enemy.x > player.x - 68 &&
@@ -89,87 +72,58 @@ class Player {
                 player.y = player.initialY;
             }
         });
-//        if (this.y)
-//        this.y += vertical;
-//        player.handleInput();
     }
 
-//        this.x += this.speed * dt;
-//    }
-
+    //  Draw the player on the screen
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
+    //  Take input from keyup event listener below and
+    //  move character by 1 block for each direction
     handleInput(key) {
         switch(key) {
             case 'up':
-//                this.update(-90, 0);
                 if (this.y <= 39) {
+                    //  Send player back to initial position on reaching water block
                     this.x = this.initialX;
                     this.y = this.initialY;
-//                    console.log(this.x, this.y);
                 } else {
-                    this.y -= 84;
+                    this.y -= 84;//  Move player up by one block
                 }
-//                this.y <= -45 ? (this.x, this.y = this.initialX, this.initialY) : this.y -= 84;
                 break;
-//                this.update();
-//                break;
             case 'down':
-//                this.update(90, 0);
-//                return 90;
+                // Prevent player to move past the last bottom block
                 if (this.y < 375) {
-                    this.y += 84;
+                    this.y += 84;//  Move player down by one block
                 }
-//                this.update();
                 break;
             case 'left':
-//                this.update(-100);
-//                return -100;
+                // Prevent player to move past the last left block
                 if (this.x > 0) {
-                    this.x -= 100;
+                    this.x -= 100;//  Move player left by one block
                 }
-//                this.update();
                 break;
             case 'right':
-//                this.update(100);
-//                return 100;
+                // Prevent player to move past the last right block
                 if (this.x < 400) {
-                    this.x += 100;
+                    this.x += 100;//  Move player right by one block
                 }
-//                this.update();
         }
-        console.log(this.x, this.y);
     }
 }
 
+//  Instantiate objects
 let allEnemies = [];
 const enemy1 = new Enemy();
 const enemy2 = new Enemy();
 const enemy3 = new Enemy();
 const enemy4 = new Enemy();
 const enemy5 = new Enemy();
-
 allEnemies.push(enemy1, enemy2, enemy3, enemy4, enemy5);
-
-
-
 const player = new Player();
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-//collision player 200, 207
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+//  Listen for key presses and send keys to Player.handleInput() method
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -179,5 +133,4 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
-//    console.log(allowedKeys[e.keyCode]);
 });
